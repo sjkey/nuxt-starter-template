425e8f4be698b080052d844295deeffc3d3af8da
sjkey <kuba.serwin99@gmail.com>
2024-09-25 16:05:19 +0200

    chore: antfu eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,51 @@
-import sjkeyConfig from '@sjkey/eslint-config-vue'
+import antfu from '@antfu/eslint-config'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  sjkeyConfig({
+  antfu({
+    lessOpinionated: true,
+
+    vue: {
+      overrides: {
+        'vue/operator-linebreak': ['error', 'before'],
+        'vue/custom-event-name-casing': ['error', 'kebab-case'],
+        'vue/component-name-in-template-casing': [
+          'error',
+          'PascalCase',
+          {
+            registeredComponentsOnly: false,
+          },
+        ],
+      },
+    },
+
+    typescript: {
+      overrides: {
+        'ts/consistent-type-definitions': ['error', 'type'],
+        'ts/array-type': [
+          'error',
+          {
+            default: 'array-simple',
+            readonly: 'array-simple',
+          },
+        ],
+        'ts/member-ordering': [
+          'error',
+          {
+            default: {
+              optionalityOrder: 'required-first',
+            },
+          },
+        ],
+      },
+    },
+
+    stylistic: {
+      overrides: {
+        curly: 'off',
+      },
+    },
+  }, {
     files: ['components/ui/**/*.ts'],
     rules: {
       'max-len': 'off',
